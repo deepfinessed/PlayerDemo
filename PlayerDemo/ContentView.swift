0aa725bce41002baad5a35e358fa58e8020c748c
Nate Munger <nate.munger@rhombus.com>
2024-02-28 14:22:24 -0800

    Work

--- PlayerDemo/ContentView.swift
+++ PlayerDemo/ContentView.swift
@@ -8,11 +8,15 @@
 import SwiftUI
 import AVKit
 
-func getVideoUrl() async throws -> URL {
+let URLS = ["https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8",
+            "https://cdn.jwplayer.com/manifests/pZxWPRg4.m3u8"]
+
+func getVideoUrl(id: Int) async throws -> URL {
     let delay = Double.random(in: 0..<4);
     NSLog("Returning URL after %f seconds", delay);
     try await Task.sleep(nanoseconds: UInt64(delay) * NSEC_PER_SEC);
-    return URL(string: "https://test-streams.mux.dev/x36xhzz/x36xhzz.m3u8")!
+    
+    return URL(string: URLS[id])!
 }
 
 struct ContentView: View {
@@ -45,7 +49,7 @@ struct ContentView: View {
                         }
                 }.task {
                     do {
-                        let url = try await getVideoUrl();
+                        let url = try await getVideoUrl(id: 0);
                         playerOne.replaceCurrentItem(with: AVPlayerItem(url: url));
                         playerOne.play()
                         
@@ -54,7 +58,7 @@ struct ContentView: View {
                     }
                 }.task {
                     do {
-                        let url = try await getVideoUrl();
+                        let url = try await getVideoUrl(id: 0);
                         playerTwo.replaceCurrentItem(with: AVPlayerItem(url: url));
                         playerTwo.play()
                         
@@ -63,8 +67,9 @@ struct ContentView: View {
                     }
                 }
             }
-            
-            Text("These players play out of sync. Please do your best to synchronize them.")
+            Text("1. These players play out of sync. Please do your best to synchronize their start.")
+            Text("2. If you accomplish this, and you would like a challenge, implement a toggle that swaps the players between the URL above and the URL with id 1, restarting playback each time.")
+            Text("3. You need not consider buffering, but in the event that you wanted to be able to synchronize the players despite one of them buffering while the other does not, describe how you might do this")
         }
         .padding()
     }
