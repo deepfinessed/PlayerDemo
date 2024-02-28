80b81ad0a53f0412c337f6f17305123a166a0877
Nate Munger <nate.munger@rhombus.com>
2024-02-28 13:20:59 -0800

    Initial Commit

---
+++ PlayerDemo/PlayerDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlayerDemoApp.swift
+//  PlayerDemo
+//
+//  Created by Nathaniel Munger on 2/28/24.
+//
+
+import SwiftUI
+
+@main
+struct PlayerDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
